5a28ef11642c37f5cce9c05842cb21f3a02a26bb
ersa <saksham14sg@gmail.com>
2025-11-24 11:42:42 +0530

    updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen">
+
+            {/* Light mode and dark mode*/}
+
+
+
+            {/* BG Effects */}
+
+
+
+            {/* Navbar */}
+
+
+
+            {/* Main Content */}
+
+
+
+            {/* Footer */}
+
+        </div>
+    )
+}
